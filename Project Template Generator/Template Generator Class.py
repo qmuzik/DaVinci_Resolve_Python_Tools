be84b58da2a7946566640f53601748c147b3bbc8
Quincy Muzik <quincymuzik0116@gmail.com>
2024-10-02 11:33:25 -0500

    Created Outline of Script
    Added Outline and TODO future updates.

--- Project Template Generator/Template Generator Class.py
+++ Project Template Generator/Template Generator Class.py
@@ -1,4 +1,21 @@
-# Project Template Generator Class Version 1.0 
+#!/usr/bin/env python
+
+# Project Template Generator Version 1.0 
 # By Quincy Muzik
 # App that allows a user to have automatic setup workspaces for DaVinci Resolve
 
+# API's for DaVinci Resolve
+
+
+# Outline of Program Steps:
+# 1. Script Runs (Either a Tool shelf or via Terminal)
+# 2. User can select a project template type with different premade workspaces/media in media pool and timeline
+# 3. User came name new project
+# 4. Project will open with new media and pre-determined settings
+
+
+# TODO FUTURE Updates:
+# Have a GUI to make it easier for the user
+# Allow the user to create a new template within the script
+# Allow the user to delete pre existing templates
+# Allow the user to select media import for template
