da6c3fa79ef11db17395330baaed089f03ccab15
Quincy Muzik <118142080+qmuzik@users.noreply.github.com>
2024-10-23 13:16:30 -0500

    Restarted Project

---
+++ Template Generator/TemplateGeneratorDaVinciResolve.py
@@ -0,0 +1,9 @@
+# Template Generator for DaVinci Resolve 1.0
+# By Quincy Muzik
+# Allows a user to select premade template that contains starting files, settings, and layout
+
+import sys
+import os
+
+ui = fusion.UIManager()
+dispatcher = bmd.UIDispatcher(ui)
