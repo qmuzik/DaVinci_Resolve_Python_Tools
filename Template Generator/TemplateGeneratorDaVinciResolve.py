e6d43c76648b1bc8bc048b32672f35f780579fab
Quincy Muzik <quincymuzik0116@gmail.com>
2024-11-08 10:52:28 -0600

    Created Layout for Template Generator
    Wrote classes, universal font, and TODO for next steps.

--- Template Generator/TemplateGeneratorDaVinciResolve.py
+++ Template Generator/TemplateGeneratorDaVinciResolve.py
@@ -7,3 +7,22 @@ import os
 
 ui = fusion.UIManager()
 dispatcher = bmd.UIDispatcher(ui)
+
+# Universal header font for the whole project 
+headerFont = ui.Font({'Family': 'Helvetica', 'PointSize': 16, 'Bold': True})
+
+#class NameProject:
+
+
+
+#class ProjectType:
+
+
+
+#class ImportFiles:
+
+
+# TODO 
+# 1. Create the main the UI
+# 2. Create a class for each function in the UI
+# 3. Check for existing instance 
