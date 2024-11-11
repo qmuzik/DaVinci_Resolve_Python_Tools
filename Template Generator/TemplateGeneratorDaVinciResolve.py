4b83960b5de5a6b690f231faa76b837318a10f4a
Quincy Muzik <quincymuzik0116@gmail.com>
2024-11-11 11:06:59 -0600

    Created Name Project Class

--- Template Generator/TemplateGeneratorDaVinciResolve.py
+++ Template Generator/TemplateGeneratorDaVinciResolve.py
@@ -11,9 +11,24 @@ dispatcher = bmd.UIDispatcher(ui)
 # Universal header font for the whole project 
 headerFont = ui.Font({'Family': 'Helvetica', 'PointSize': 16, 'Bold': True})
 
-#class NameProject:
+class NameProject:
+    # Constructor
+    def __init__(self):
+        self.name = f'name'
 
-
+    # Creates the UI Layout
+    def get_ui(self):
+        return ui.VGroup({'Weight':0.1, 'Spacing': 5, }, [
+            ui.Label({'Text': 'Please name your project: ', 'Font': headerFont, 'Weight': 0}),
+            ui.HGroup({ 'Weight': 0 }, [
+                ui.TextEdit({'ID': self.name, 'Text': "Project Name"})
+                ui.HGap(0,0.9),
+            ]),
+            ui.VGap(0,0.05)
+        ])
+    
+    # Event Handlers for UI
+    
 
 #class ProjectType:
 
@@ -25,4 +40,4 @@ headerFont = ui.Font({'Family': 'Helvetica', 'PointSize': 16, 'Bold': True})
 # TODO 
 # 1. Create the main the UI
 # 2. Create a class for each function in the UI
-# 3. Check for existing instance 
+# 3. Check for existing instance
